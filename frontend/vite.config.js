f83806f8715a6871eda79a92c5a7bbacc0cb1013
benjamin.nativel <benjaminhenri.nativel31@gmail.com>
2026-05-05 15:59:26 +0200

    fix sur le vite.config pour la MEP

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  server: {
+    hmr:
+      mode === 'production'
+        ? false
+        : {
+            host: '10.10.40.220',
+            port: 80,
+          },
+  },
+}))
